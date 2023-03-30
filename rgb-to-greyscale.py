f324638063209a1b72ee8ab281861b2f1b3995fc
Shubh Raheja <90206013+shubh1176@users.noreply.github.com>
2023-03-30 11:05:10 +0530

    Add files via upload

--- rgb-to-greyscale.py
+++ rgb-to-greyscale.py
@@ -1,5 +1,9 @@
-from PIL import Image
+from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
 
-img = Image.open('test.jpg')
-imgGray = img.convert('L')
-imgGray.save('test_gray.jpg')
+img = mpimg.imread('test.jpg')
+
+R, G, B = img[:,:,0], img[:,:,1], img[:,:,2]
+imgGray = 0.2989 * R + 0.5870 * G + 0.1140 * B
+plt.imshow(imgGray, cmap='gray')
+plt.show()
